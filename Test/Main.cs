f0cf7b4c9dd2cf73138388fa9aabd945eab056a9
hryknkgw <hryknkgw@gmail.com>
2012-09-05 13:30:36 +0900

    Add test files (2GK0)

--- Test/Main.cs
+++ Test/Main.cs
@@ -7,7 +7,7 @@ namespace Test
 		public static void Main (string[] args)
 		{
 			var p = new Molecule.PDBParser ();
-			var s = p.Parse ("2J88.pdb");
+			var s = p.Parse ("2GK0.pdb");
 			System.Console.WriteLine (s);
 		}
 	}
